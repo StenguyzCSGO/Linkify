5390b220477232184b89ca49405e61839e14cbd0
StenguyzCSGO <Stenguyz@gmail.com>
2025-07-21 16:54:08 +0200

    chore(poise): follow the quickstart guide from poise framework and organize well the commands

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,36 @@
-use serenity::prelude::*;
-use serenity::utils::token::validate;
-use serenity::async_trait;
-use serenity::model::channel::Message;
+mod commands;
+
+use poise::serenity_prelude as serenity;
+use dotenv::dotenv;
 use std::env;
-use std::process;
-use serenity::gateway::ShardManager;
-use std::sync::Arc;
-use serenity::prelude::TypeMapKey;
 
-struct Handler;
-struct ShardManagerContainer;
-
-impl TypeMapKey for ShardManagerContainer {
-    type Value = Arc<ShardManager>;
-}
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            let data_read = ctx.data.read().await;
-            let manager = data_read
-                .get::<ShardManagerContainer>()
-                .expect("ShardManager not found");
-
-            let runners = manager.runners.lock().await;
-            let runner = runners
-                .get(&ctx.shard_id)
-                .expect("No shard runner for current shard");
-
-            let reply = match runner.latency {
-                Some(duration) => format!("{} ms", duration.as_millis()),
-                None => "Not yet known".to_string(),
-            };
-
-            if let Err(why) = msg.channel_id.say(&ctx.http, reply).await {
-                println!("Error sending message: {why:?}");
-             }
-        }
-    }
-}
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let discord_token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not set");
-    if validate(&discord_token).is_err() {
-        eprintln!("Invalid Discord token");
-        process::exit(1);
-    }
+    let intents = serenity::GatewayIntents::non_privileged();
 
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT; 
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![
+                commands::ping::ping()
+            ],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
 
-    let mut client = Client::builder(&discord_token, intents)
-        .event_handler(Handler)
-        .await
-        .expect("Error when creating client");
-
-    let shard_manager = client.shard_manager.clone();
-    {
-        let mut data = client.data.write().await;
-        data.insert::<ShardManagerContainer>(shard_manager);
-    }
-        
-
-    if let Err(why) = client.start().await {
-        println!("Client error: {why:?}");
-    }    
+    let client = serenity::ClientBuilder::new(discord_token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
 }
