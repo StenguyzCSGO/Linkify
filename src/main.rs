4419a11f829baebdb0dfad97ac3ae5314d3a7024
CLOS Merlin <92889964+StenguyzCSGO@users.noreply.github.com>
2025-07-23 09:45:17 +0200

    Refactor/command (#7)
    * chore(poise): follow the quickstart guide from poise framework and organize well the commands
    
    * chore(guild): register a guild id to test the new commands
    
    * chore: add others main files
    
    * refactor: copilot suggestions

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,53 @@
-use serenity::prelude::*;
-use serenity::utils::token::validate;
-use serenity::async_trait;
-use serenity::model::channel::Message;
+mod commands;
+
+use poise::serenity_prelude as serenity;
+use dotenv::dotenv;
 use std::env;
-use std::process;
-use serenity::gateway::ShardManager;
-use std::sync::Arc;
-use serenity::prelude::TypeMapKey;
 
-struct Handler;
-struct ShardManagerContainer;
-
-impl TypeMapKey for ShardManagerContainer {
-    type Value = Arc<ShardManager>;
-}
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            let data_read = ctx.data.read().await;
-            let manager = data_read
-                .get::<ShardManagerContainer>()
-                .expect("ShardManager not found");
-
-            let runners = manager.runners.lock().await;
-            let runner = runners
-                .get(&ctx.shard_id)
-                .expect("No shard runner for current shard");
-
-            let reply = match runner.latency {
-                Some(duration) => format!("{} ms", duration.as_millis()),
-                None => "Not yet known".to_string(),
-            };
-
-            if let Err(why) = msg.channel_id.say(&ctx.http, reply).await {
-                println!("Error sending message: {why:?}");
-             }
-        }
-    }
-}
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let discord_token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not set");
-    if validate(&discord_token).is_err() {
-        eprintln!("Invalid Discord token");
-        process::exit(1);
+    let guild_id = env::var("GUILD_ID")
+        .ok()
+        .and_then(|id| id.parse::<u64>().ok())
+        .map(serenity::GuildId::new);
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![
+                commands::ping::ping()
+            ],
+            ..Default::default()
+        })
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                if let Some(guild_id) = guild_id {
+                    poise::builtins::register_in_guild(ctx, &framework.options().commands, guild_id).await?;
+                    println!("Development mode");
+                } else {
+                    poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                    println!("Production mode");
+                }
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(discord_token, intents)
+        .framework(framework)
+        .await;
+
+    match client {
+        Ok(mut client) => match client.start().await {
+            Ok(_) => (),
+            Err(e) => eprintln!("Failed to start Serenity Client: {}", e),
+        },
+        Err(e) => eprintln!("Failed to create Serenity Client: {}", e),
     }
-
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT; 
-
-    let mut client = Client::builder(&discord_token, intents)
-        .event_handler(Handler)
-        .await
-        .expect("Error when creating client");
-
-    let shard_manager = client.shard_manager.clone();
-    {
-        let mut data = client.data.write().await;
-        data.insert::<ShardManagerContainer>(shard_manager);
-    }
-        
-
-    if let Err(why) = client.start().await {
-        println!("Client error: {why:?}");
-    }    
 }
